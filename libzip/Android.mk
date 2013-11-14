245703ba3a573262a68cdd90b8503efa48af0eb5
Benjamin Eikel <benjamin@eikel.org>
2013-11-14 11:38:22 +0100

    Import pre-built Android libraries
    from SVN repository revision 297.

---
+++ libzip/Android.mk
@@ -0,0 +1,17 @@
+#
+#	This file is part of the OpenGLCore library.
+#	Copyright (C) 2013 Benjamin Eikel <benjamin@eikel.org>
+#	
+#	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+#	You should have received a copy of the MPL along with this library; see the 
+#	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libzip-prebuilt
+LOCAL_SRC_FILES := lib/libzip.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include \
+                           $(LOCAL_PATH)/lib/libzip/include
+include $(PREBUILT_STATIC_LIBRARY)
