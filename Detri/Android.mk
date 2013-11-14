56d7b9214de69e470bfcc2256369c33d5429913b
Benjamin Eikel <benjamin@eikel.org>
2013-11-14 12:02:51 +0100

    Update copyright/license headers
    Rename the project to PADrendMobile.

--- Detri/Android.mk
+++ Detri/Android.mk
@@ -1,10 +1,11 @@
 #
-#	This file is part of the OpenGLCore library.
-#	Copyright (C) 2013 Benjamin Eikel <benjamin@eikel.org>
-#	
-#	This library is subject to the terms of the Mozilla Public License, v. 2.0.
-#	You should have received a copy of the MPL along with this library; see the 
-#	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+# This file is part of the PADrendMobile project.
+# Web page: http://www.padrend.de/
+# Copyright (C) 2013 Benjamin Eikel <benjamin@eikel.org>
+#
+# This project is subject to the terms of the Mozilla Public License, v. 2.0.
+# You should have received a copy of the MPL along with this project; see the
+# file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
 LOCAL_PATH := $(call my-dir)
